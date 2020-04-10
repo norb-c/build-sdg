a6e38c5dfe15993ba3fa89ae5f4a343c4c5c214c
chigozie3911 <chigozie@lendsqr.com>
2020-04-10 17:56:52 +0100

    fixing periods

--- src/estimator.js
+++ src/estimator.js
@@ -17,8 +17,20 @@
 //    impact: {}, // your best case estimation
 //    severeImpact: {} // your severe case estimation
 // }
-function infectionsByRequestedTime(currentlyInfected, days) {
-  const exp = days / 3;
+function infectionsByRequestedTime(
+  currentlyInfected,
+  timeToElapse,
+  periodType
+) {
+  let exp;
+  if (periodType === 'days') {
+    exp = timeToElapse / 3;
+  } else if (periodType === 'weeks') {
+    exp = (timeToElapse * 7) / 3;
+  } else {
+    exp = (timeToElapse * 30) / 3;
+  }
+
   return currentlyInfected * 2 ** exp;
 }
 
@@ -34,12 +46,14 @@ const covid19ImpactEstimator = (data) => {
   // infected for 30 days (could be a function that takes days)
   finalOutput.impact.infectionsByRequestedTime = infectionsByRequestedTime(
     finalOutput.impact.currentlyInfected,
-    30
+    data.timeToElapse,
+    data.periodType
   );
 
   finalOutput.servereImpact.infectionsByRequestedTime = infectionsByRequestedTime(
     finalOutput.servereImpact.currentlyInfected,
-    30
+    data.timeToElapse,
+    data.periodType
   );
 
   return finalOutput;
