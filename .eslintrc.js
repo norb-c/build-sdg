adceeee17e51d760f38ead7509f82be18cd9bb2e
chigozie3911 <chigozie@lendsqr.com>
2020-04-11 14:55:19 +0100

    checking build

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
+
   rules: {
     'comma-dangle': ['error', 'never'],
     'no-param-reassign': ['error', { props: false }],
